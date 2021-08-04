b616db198e40425c8c459dfbebfdbccc3bd30ec4
Art <d.art.shishkin@gmail.com>
2021-08-04 17:28:03 +0300

    11. Creating Loops and Arrays - Part 1 (#1)

---
+++ typescript-training/02-variables/sample-types.js
@@ -0,0 +1,19 @@
+var myBool = true;
+var myNumber = 123.45;
+var myString1 = "Double quotes";
+var myString2 = 'Single quotes';
+//let's break it
+// myBool = 0;
+// myNumber = "A";
+// myString1 = false;
+console.log(myBool);
+console.log('My number is: ' + myNumber);
+console.log("String template1: " + myNumber);
+console.log("String template2: " + myString1 + " and " + myString2);
+var anyVar;
+anyVar = myBool;
+console.log("anyVar: " + anyVar);
+anyVar = myNumber;
+console.log("anyVar: " + anyVar);
+anyVar = myString1;
+console.log("anyVar: " + anyVar);
