de6ccc16d83e9608e3f57b7902bd17f2b10edc4c
Art <d.art.shishkin@gmail.com>
2021-08-05 18:54:03 +0300

    48. Create Spring Boot Starter Project (#7)

---
+++ spring-boot-ecommerce/src/main/java/net/shyshkin/study/fullstack/ecommerce/SpringBootEcommerceApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.fullstack.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEcommerceApplication.class, args);
+    }
+
+}
