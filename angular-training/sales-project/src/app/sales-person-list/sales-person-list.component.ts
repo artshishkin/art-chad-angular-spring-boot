9ab55a96712c4745aadc500e3948ebfb744da346
Art <d.art.shishkin@gmail.com>
2021-08-05 12:14:19 +0300

    37. Creating a New Angular Component - Part 2 (#4)

--- angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SalesPerson} from "./sales-person";
 
 @Component({
   selector: 'app-sales-person-list',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesPersonListComponent implements OnInit {
 
-  constructor() { }
+  salesPersonList: SalesPerson[] = [];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.salesPersonList.push(
+      new SalesPerson('Art', 'Shyshkin', 'art@shyshkin.com', 123.45),
+      new SalesPerson('Kate', 'Shyshkina', 'kate@shyshkin.com', 234.56),
+      new SalesPerson('Arina', 'Shyshkina', 'arina@shyshkin.com', 345.67),
+      new SalesPerson('Nazar', 'Shyshkin', 'nazar@shyshkin.com', 456.78)
+    );
   }
 
 }
