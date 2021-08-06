f88c1d06c35b79d5de75a67292508d8bef7980cb
Art <d.art.shishkin@gmail.com>
2021-08-06 12:01:36 +0300

    57. Angular Project - Create Product class (#8)

---
+++ angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+
+  sku: string;
+  name: string;
+  description: string;
+  unitPrice: number;
+  imageUrl: string;
+  active: Boolean;
+  dateCreated: Date;
+  lastUpdated: Date;
+
+}
