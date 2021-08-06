ee339673f1b8b4017a731c8825cf2a0db52c32f6
Art <d.art.shishkin@gmail.com>
2021-08-06 19:05:18 +0300

    9.2.3 Temporarily enable cross origin from everywhere (#9)

--- spring-boot-ecommerce/src/main/java/net/shyshkin/study/fullstack/ecommerce/repositotry/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/net/shyshkin/study/fullstack/ecommerce/repositotry/ProductRepository.java
@@ -4,7 +4,7 @@ import net.shyshkin.study.fullstack.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-//@CrossOrigin //from everywhere
-@CrossOrigin({"http://localhost:4200","http://angular-ecommerce:4200"})
+@CrossOrigin //from everywhere
+//@CrossOrigin({"http://localhost:4200","http://angular-ecommerce:4200"})
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
