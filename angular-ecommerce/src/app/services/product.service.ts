282a17f90ee46a3afc1437d0407d0caab6a1d4b2
Art <d.art.shishkin@gmail.com>
2021-08-08 00:02:46 +0300

    80. Angular Project - Search for Products by Category - Code - Update ProductService (#11)

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -16,12 +16,13 @@ export class ProductService {
   constructor(private httpClient: HttpClient) {
   }
 
-    getProductList(currentCategoryId: number): Observable<Product[]> {
+  getProductList(categoryId: number): Observable<Product[]> {
 
-    // @TODO:  need to build URL based on category id
+    // need to build URL based on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
 
     return this.httpClient
-      .get<GetResponse>(this.baseUrl)
+      .get<GetResponse>(searchUrl)
       .pipe(map(response => response._embedded.products));
   }
 }
