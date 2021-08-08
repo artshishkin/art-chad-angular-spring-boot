161da09b838ec6a453dc4e23c871ef8c8152d85c
Art <d.art.shishkin@gmail.com>
2021-08-08 13:19:00 +0300

    88. Bonus: Display Category Name in Product List Grid (#12)

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,10 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number;
+  currentCategoryName: string;
+
+  private DEFAULT_CATEGORY_ID: number = 1;
+  private DEFAULT_CATEGORY_NAME: string = 'Books';
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) {
@@ -26,8 +30,21 @@ export class ProductListComponent implements OnInit {
 
   private listProducts() {
 
-    const categoryId = this.route.snapshot.paramMap.get('id');
-    this.currentCategoryId = (categoryId != null) ? +categoryId : 1;
+    const hasCategoryId = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      const categoryId = this.route.snapshot.paramMap.get('id');
+      this.currentCategoryId = (categoryId != null) ? +categoryId : this.DEFAULT_CATEGORY_ID;
+
+      const categoryName = this.route.snapshot.paramMap.get('name');
+      this.currentCategoryName = categoryName != null ? categoryName : this.DEFAULT_CATEGORY_NAME;
+
+    } else {
+
+      this.currentCategoryId = this.DEFAULT_CATEGORY_ID;
+      this.currentCategoryName = this.DEFAULT_CATEGORY_NAME;
+
+    }
 
     this.productService.getProductList(this.currentCategoryId).subscribe(data => this.products = data);
   }
