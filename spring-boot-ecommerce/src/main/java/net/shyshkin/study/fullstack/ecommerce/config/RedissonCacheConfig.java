54cf4b4639a265e54bb587a6de220dc0bd6253e3
Art <d.art.shishkin@gmail.com>
2021-08-08 15:45:22 +0300

    13.5 Profile `no_cache` (#13)

--- spring-boot-ecommerce/src/main/java/net/shyshkin/study/fullstack/ecommerce/config/RedissonCacheConfig.java
+++ spring-boot-ecommerce/src/main/java/net/shyshkin/study/fullstack/ecommerce/config/RedissonCacheConfig.java
@@ -2,13 +2,18 @@ package net.shyshkin.study.fullstack.ecommerce.config;
 
 import org.redisson.api.RedissonClient;
 import org.redisson.spring.cache.RedissonSpringCacheManager;
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableCaching
+@Profile("!no_cache")
+@Import({RedissonAutoConfiguration.class})
 public class RedissonCacheConfig {
 
     @Bean
