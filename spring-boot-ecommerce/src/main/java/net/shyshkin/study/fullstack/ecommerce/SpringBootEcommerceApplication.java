7448f77bf35026bc987031319d86e72362a6d94d
Art <d.art.shishkin@gmail.com>
2021-08-08 17:22:47 +0300

    13.6 Profile `no_cache` - exclude RedissonAutoConfiguration with application.yml (#13)

--- spring-boot-ecommerce/src/main/java/net/shyshkin/study/fullstack/ecommerce/SpringBootEcommerceApplication.java
+++ spring-boot-ecommerce/src/main/java/net/shyshkin/study/fullstack/ecommerce/SpringBootEcommerceApplication.java
@@ -1,10 +1,10 @@
 package net.shyshkin.study.fullstack.ecommerce;
 
-import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {RedissonAutoConfiguration.class})
+@SpringBootApplication
+//@SpringBootApplication(exclude = {RedissonAutoConfiguration.class})
 public class SpringBootEcommerceApplication {
 
     public static void main(String[] args) {
