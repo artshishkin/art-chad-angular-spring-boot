fa3d50ecee91e33a4539edb611c11a6af07c1d29
Art <d.art.shishkin@gmail.com>
2021-08-10 23:11:47 +0300

    143. Angular Project - Increment Item Quantity - Add Event Handlers (#20)

--- angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -25,4 +25,8 @@ export class CartDetailsComponent implements OnInit {
     this.cartService.cartStatusSubject.asObservable().subscribe(data => this.cartTotals = data);
     this.cartService.computeCartTotals();
   }
+
+  incrementQuantity(cartItem: CartItem) {
+    this.cartService.addToCart(cartItem);
+  }
 }
