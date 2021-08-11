f67b3474795a397f5425168d06b53e5e996a6e42
Art <d.art.shishkin@gmail.com>
2021-08-11 22:44:32 +0300

    171. Angular Project - Checkout Form - Populate Countries and States - FormService (#22)

--- angular-ecommerce/src/app/services/my-shop-form.service.ts
+++ angular-ecommerce/src/app/services/my-shop-form.service.ts
@@ -1,12 +1,21 @@
 import {Injectable} from '@angular/core';
 import {Observable, of} from "rxjs";
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Country} from "../common/country";
+import {map} from "rxjs/operators";
+import {State} from "../common/state";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyShopFormService {
 
-  constructor() {
+  private baseUrl = `${environment.backendServerUrl}/api`;
+  private countriesUrl = `${this.baseUrl}/countries`;
+  private statesUrl = `${this.baseUrl}/states`;
+
+  constructor(private httpClient: HttpClient) {
   }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
@@ -30,4 +39,27 @@ export class MyShopFormService {
     return of(years);
   }
 
+  getCountries(): Observable<Country[]> {
+    return this.httpClient
+      .get<CountriesGetResponse>(this.countriesUrl)
+      .pipe(map(data => data._embedded.countries));
+  }
+
+  getStates(countryCode: string): Observable<State[]> {
+    let searchUrl = `${this.statesUrl}/search/by-country-code?code=${countryCode}`;
+    return this.httpClient.get<StatesGetResponse>(searchUrl)
+      .pipe(map(data => data._embedded.states));
+  }
+}
+
+interface CountriesGetResponse {
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface StatesGetResponse {
+  _embedded: {
+    states: State[];
+  }
 }
