85704fdc96d9ead111cb124fd0b11c03e1fdf240
Art <d.art.shishkin@gmail.com>
2021-08-12 12:41:21 +0300

    Modified repositories to block `/api/states` endpoint(#24)

--- spring-boot-ecommerce/src/main/java/net/shyshkin/study/fullstack/ecommerce/repositotry/CountryRepository.java
+++ spring-boot-ecommerce/src/main/java/net/shyshkin/study/fullstack/ecommerce/repositotry/CountryRepository.java
@@ -1,7 +1,10 @@
 package net.shyshkin.study.fullstack.ecommerce.repositotry;
 
 import net.shyshkin.study.fullstack.ecommerce.entity.Country;
+import net.shyshkin.study.fullstack.ecommerce.entity.CountryWithoutStates;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(excerptProjection = CountryWithoutStates.class)
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 }
--- spring-boot-ecommerce/src/main/java/net/shyshkin/study/fullstack/ecommerce/repositotry/StateRepository.java
+++ spring-boot-ecommerce/src/main/java/net/shyshkin/study/fullstack/ecommerce/repositotry/StateRepository.java
@@ -1,15 +1,27 @@
 package net.shyshkin.study.fullstack.ecommerce.repositotry;
 
 import net.shyshkin.study.fullstack.ecommerce.entity.State;
+import net.shyshkin.study.fullstack.ecommerce.entity.StateWithoutCountry;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(excerptProjection = StateWithoutCountry.class)
 public interface StateRepository extends JpaRepository<State, Integer> {
 
-    @RestResource(path = "by-country-code")
-    List<State> findByCountryCode(@Param("code") String code);
+    @Override
+    @RestResource(exported = false)
+    List<State> findAll();
+
+    @Override
+    @RestResource(exported = false)
+    Page<State> findAll(Pageable pageable);
+
+//    @RestResource(path = "by-country-code")
+//    List<State> findByCountryCode(@Param("code") String code);
 
 }
