79778d3a0f4cbc70f2a184c21e93cb3e14e05ea8
Art <d.art.shishkin@gmail.com>
2021-09-01 16:57:55 +0300

    259. Release 3.0 - Security - Order History - Frontend Coding - OrderHistory Service (#37)

---
+++ angular-ecommerce/src/app/services/order-history.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {OrderHistory} from "../common/order-history";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderHistoryService {
+
+  private orderUrl = `${environment.backendServerUrl}/api/orders`;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getOrderHistory(email: string): Observable<GetResponseOrderHistory> {
+    const orderHistoryUrl = `${this.orderUrl}/search/by-customer-email?email=${email}`;
+    return this.httpClient.get<GetResponseOrderHistory>(orderHistoryUrl);
+  }
+
+}
+
+interface GetResponseOrderHistory {
+  _embedded: {
+    orders: OrderHistory[];
+  }
+}
