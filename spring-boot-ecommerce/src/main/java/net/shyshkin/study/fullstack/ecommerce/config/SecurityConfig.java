d2bffb527a3d8dd3c2d59794ceca8fae38ba6cc7
Art <d.art.shishkin@gmail.com>
2021-09-02 10:38:01 +0300

    268. Release 3.0 - Security - Order History - Secure Backend - Protect Endpoints (#38)

---
+++ spring-boot-ecommerce/src/main/java/net/shyshkin/study/fullstack/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package net.shyshkin.study.fullstack.ecommerce.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // protect endpoint `/api/orders`
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        // add CORS filters
+        http.cors();
+    }
+}
