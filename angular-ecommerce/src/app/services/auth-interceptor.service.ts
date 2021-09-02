becd6f47a2b6b97474d592d18bd3dfb3f2118817
Art <d.art.shishkin@gmail.com>
2021-09-02 13:08:23 +0300

    273. Pass Access Token (#39)

--- angular-ecommerce/src/app/services/auth-interceptor.service.ts
+++ angular-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {from, Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+import {OktaAuthService} from "@okta/okta-angular";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor() {
+  constructor(private oktaAuthService: OktaAuthService) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -18,6 +20,22 @@ export class AuthInterceptorService implements HttpInterceptor {
 
   private async handleAccess(req: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
 
-    throw new Error("NOT IMPLEMENTED YET");
+    // Only add access token to secured endpoint
+    const securedEndpoints = [`${environment.backendServerUrl}/api/orders`];
+
+    if (securedEndpoints.some(url => req.urlWithParams.includes(url))) {
+      //get access token
+      // const accessToken = this.oktaAuthService.getAccessToken();
+      const accessToken = await this.oktaAuthService.getAccessToken();
+      //todo
+
+      //clone the request and add new header with access token
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+    }
+    return next.handle(req).toPromise();
   }
 }
