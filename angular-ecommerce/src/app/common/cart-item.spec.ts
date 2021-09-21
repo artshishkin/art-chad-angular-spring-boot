8114465846e04aae79c98365eb889019b8c70f9d
Art <d.art.shishkin@gmail.com>
2021-09-22 01:39:15 +0300

    Cart Item simple test (study purpose)

--- angular-ecommerce/src/app/common/cart-item.spec.ts
+++ angular-ecommerce/src/app/common/cart-item.spec.ts
@@ -1,7 +1,13 @@
 import {CartItem} from './cart-item';
+import {Product} from "./product";
 
 describe('CartItem', () => {
   it('should create an instance', () => {
-    expect(new CartItem()).toBeTruthy();
+    const product = new Product();
+    product.id = 123;
+    product.name = 'Ford';
+    product.unitPrice = 12500;
+    product.imageUrl = "http://localhost:4200/product/123/image";
+    expect(new CartItem(product)).toBeTruthy();
   });
 });
